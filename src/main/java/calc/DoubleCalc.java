f584a8fda57d9a0a415032015f00972884eeb955
nini <nini.tran@outlook.com>
2022-09-09 23:20:51 +0200

    power method

--- src/main/java/calc/DoubleCalc.java
+++ src/main/java/calc/DoubleCalc.java
@@ -10,13 +10,14 @@ public class DoubleCalc {
         final BufferedReader terminalSource = new BufferedReader(new InputStreamReader(System.in));
         double a = Double.parseDouble(terminalSource.readLine());
         double b = Double.parseDouble(terminalSource.readLine());
-       // String operator = terminalSource.readLine();
+        String operator = terminalSource.readLine();
 
    //     System.out.println(add(a, b));
 //        System.out.println(substract(a, b));
 //        System.out.println(multi(a, b));
         System.out.println(divide(a, b));
-
+        System.out.println(calc(a,b,operator));
+        System.out.println(power(a,b));
     }
 
     public static double add(double number1, double number2) {
@@ -38,5 +39,30 @@ public class DoubleCalc {
         } else {
             return number1 / number2;
         }
+    }
+
+    public static double calc(double number1, double number2, String operator) {
+        switch (operator) {
+            case "+" :
+                return number1 + number2;
+            case "-" :
+                return number1 - number2;
+            case "*" :
+                return number1 * number2;
+            case "/" :
+                return number1 / number2;
+            default:
+                System.out.println("Operator not supported.");
+                return 0;
+        }
+    }
+
+    public static double power(double base, double exponent) {
+        return Math.pow(base, exponent);                        //another option (lazy version)
+//        double sum = 1;
+//        for (int i = 1; i <= exponent; i++) {
+//            sum = base * sum;
+//        }
+//        return sum;
     }
 }
--- src/main/java/dnd/Die.java
+++ src/main/java/dnd/Die.java
@@ -1,17 +1,19 @@
 package dnd;
 
 public enum Die {
-    D4(4),
-    D6(6),
-    D8(8),
-    D10(10),
-    D12(12),
-    D20(20),
-    D100(100);
+    D4(4, "4 sided die"),
+    D6(6, "6 sided die"),
+    D8(8, "8 sided die"),
+    D10(10, "10 sided die"),
+    D12(12, "12 sided die"),
+    D20(20, "20 sided die"),
+    D100(100, "100 sided die");
 
     int sides;
+    String name;
 
-    Die(int sides) {
+    Die(int sides, String name) {
         this.sides = sides;
+        this.name = name;
     }
 }
